9b7f88bc7f008346eab96160cc4d6bbf76ba196d
Paul Bayfield <137620008+PaulBayfield@users.noreply.github.com>
2026-05-30 09:50:21 +0200

    chore: bump project version

--- CrousPy/__init__.py
+++ CrousPy/__init__.py
@@ -1,6 +1,6 @@
 __title__ = "CrousPy"
 __author__ = "CROUStillant Développement"
-__version__ = "2.0.6"
+__version__ = "2.0.7"
 __description__ = "A Python wrapper for the CROUS API."
 
 __headers__ = {
